1d3dc9065da6fa2502a1caf9f776c3dc3928eaaf
Hillary Odinchefu <insanedude.developer@gmail.com>
2025-01-10 22:38:30 +0100

    :sparkles: Added dev packages

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import bodyParser from "body-parser";
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 
 export const InitApp = () => {
   const app = express();
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
+  app.use(helmet())
   app.use(
     cors({
       origin: "*",
